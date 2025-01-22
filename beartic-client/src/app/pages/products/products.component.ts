892d5fc690e1797e4bb8bfa90e27989e55f84569
Kaique Barros <kaique_rba@yahoo.com.br>
2025-01-22 17:33:58 -0300

    Card de produtos

--- beartic-client/src/app/pages/products/products.component.ts
+++ beartic-client/src/app/pages/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
